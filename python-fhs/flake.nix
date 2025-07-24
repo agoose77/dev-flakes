e100acf10b54df3e41b649b948f7a474ab30fd5b
Angus Hollands <goosey15@gmail.com>
2025-07-24 07:45:00 +0100

    feat: more Python versions

--- python-fhs/flake.nix
+++ python-fhs/flake.nix
@@ -11,22 +11,44 @@
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
-      envWithScript = script:
+      envWithScript = {
+        script,
+        python,
+        pythonPackages,
+      }:
         (pkgs.buildFHSUserEnv {
           name = "python-env";
-          targetPkgs = pkgs: (with pkgs; [
-            python3
-            python3Packages.pip
-            python3Packages.virtualenv
-            pythonManylinuxPackages.manylinux2014Package
-            cmake
-            ninja
-            gcc
-            pre-commit
-          ]);
+          targetPkgs = pkgs:
+            (with pkgs; [
+              pythonManylinuxPackages.manylinux2014Package
+              cmake
+              ninja
+              gcc
+              pre-commit
+            ])
+            ++ [python] ++ (with pythonPackages; [pip virtualenv]);
           runScript = "${pkgs.writeShellScriptBin "runScript" (''
-                     set -e
-                     test -d .venv || ${pkgs.python3.interpreter} -m venv .venv
+              set -e
+
+              # Setup if not defined ####
+              if [[ ! -f ".venv/marker" ]]; then
+                  __setup_env() {
+                      # Remove existing venv
+                      if [[ -d .venv ]]; then
+                          rm -r .venv
+                      fi
+
+                      # Stand up new venv
+                      ${python.interpreter} -m venv .venv
+
+                      # Add a marker that marks this venv as "ready"
+                      touch .venv/marker
+                  }
+
+                  __setup_env
+              fi
+              ###########################
+
               source .venv/bin/activate
               set +e
             ''
@@ -34,6 +56,28 @@
         })
         .env;
     in {
-      devShell = envWithScript "bash";
+      devShells = rec {
+        py310 = envWithScript {
+          script = "bash";
+          python = pkgs.python310;
+          pythonPackages = pkgs.python310Packages;
+        };
+        py311 = envWithScript {
+          script = "bash";
+          python = pkgs.python311;
+          pythonPackages = pkgs.python311Packages;
+        };
+        py312 = envWithScript {
+          script = "bash";
+          python = pkgs.python312;
+          pythonPackages = pkgs.python312Packages;
+        };
+        py313 = envWithScript {
+          script = "bash";
+          python = pkgs.python313;
+          pythonPackages = pkgs.python313Packages;
+        };
+        default = py313;
+      };
     });
 }
