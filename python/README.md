8dc64c0be43d92ca030b94f3a54e118060570003
Angus Hollands <goosey15@gmail.com>
2025-08-05 13:41:44 +0100

    wip: add non-FHS

---
+++ python/README.md
@@ -0,0 +1,3 @@
+# Python
+
+This flake establishes an non-FHS environment for Python 3, with support for compilation, wheels, and pre-commit. It activates the virtual-environment upon load.
---
+++ python/flake.nix
@@ -0,0 +1,75 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      inherit (pkgs) lib;
+
+      envWithScript = (
+        python: let
+          packages =
+            (with pkgs; [
+              cmake
+              ninja
+              gcc
+              pre-commit
+            ])
+            ++ [python];
+
+          shellHook = ''
+            # Unset leaky PYTHONPATH
+            unset PYTHONPATH
+
+            # Setup if not defined ####
+            if [[ ! ( -d ".venv" && -f ".venv/marker" ) ]]; then
+                __setup_env() {
+                    # Remove existing venv
+                    if [[ -d .venv ]]; then
+                        rm -r .venv
+                    fi
+
+                    # Stand up new venv
+                    ${python.interpreter} -m venv .venv
+
+                    # Add a marker that marks this venv as "ready"
+                    touch .venv/marker
+                }
+
+              __setup_env
+            fi
+            ###########################
+
+            # Activate venv
+            source .venv/bin/activate
+          '';
+          env = lib.optionalAttrs pkgs.stdenv.isLinux {
+            # Python uses dynamic loading for certain libraries.
+            # We'll set the linker path instead of patching RPATH
+            LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux2014;
+          };
+        in
+          pkgs.mkShell {
+            inherit env packages shellHook;
+          }
+      );
+    in {
+      devShells = rec {
+        py310 =
+          envWithScript pkgs.python310;
+        py311 =
+          envWithScript pkgs.python311;
+        py312 =
+          envWithScript pkgs.python312;
+        py313 =
+          envWithScript pkgs.python313;
+        default = py313;
+      };
+    });
+}
